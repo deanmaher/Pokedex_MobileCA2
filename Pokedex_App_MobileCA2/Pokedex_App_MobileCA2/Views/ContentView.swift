040ead2055e340ba814c0b575a8cfba1915ebbce
deanmaher <72203596+deanmaher@users.noreply.github.com>
2023-05-16 19:38:29 +0100

    Add files via upload

--- Pokedex_App_MobileCA2/Pokedex_App_MobileCA2/Views/ContentView.swift
+++ Pokedex_App_MobileCA2/Pokedex_App_MobileCA2/Views/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
         NavigationView {
             
             List {
+                //if search bar is blank show all pokemon
                 ForEach(searchText == "" ? pokemon: pokemon.filter( {$0.name.contains(searchText.lowercased())} )) {
                     entry in
                     
@@ -18,7 +19,7 @@ struct ContentView: View {
                         PokemonImage(imageLink: "\(entry.url)")
                             .padding(.trailing, 20)
                         
-                        NavigationLink("\(entry.name)".capitalized ,destination: Text("Detail view for \(entry.name)"))
+                        NavigationLink("\(entry.name)".capitalized ,destination: DetailView(pokemonName: entry.name))
                     }
                 }
             }
--- Pokedex_App_MobileCA2/Pokedex_App_MobileCA2/Views/DetailView.swift
+++ Pokedex_App_MobileCA2/Pokedex_App_MobileCA2/Views/DetailView.swift
@@ -6,30 +6,70 @@
 //
 import SwiftUI
 
+
+
 struct DetailView: View {
-    let pokemonEntry: PokemonEntry
-    @State var pokemonSelected: PokemonSelected?
-
+    
+    @State var sprites = [PokemonSprites]()
+    @State var pokemon = [PokemonEntry]()
+    let pokemonName: String
+    let url = "https://pokeapi.co/api/v2/pokemon/"
+    
     var body: some View {
-        VStack {
-            if let pokemonSelected = pokemonSelected {
-                // Display the Pokemon sprite and name here
-                Text(pokemonEntry.name.capitalized)
-                    .font(.largeTitle)
-                    .padding()
-                if let spriteURL = pokemonSelected.sprites.front_default {
-                    //PokemonImage(url: spriteURL)
-                    //    .padding(.bottom, 20)
-                }
-            } else {
-                Text("Loading...")
-            }
-        }
-        .onAppear {
+        // Add pokemonName to end of url to pull data from
+        let url = "https://pokeapi.co/api/v2/pokemon/"+pokemonName
+        Text("Detail view for \(pokemonName)")
+        Text("Call PokeAPI with url ending in pokemonName ->"+pokemonName)
+        
+        ForEach(pokemon.filter( {$0.name.contains(pokemonName)} )) {
+            entry in
             
+            HStack {
+                PokemonImage(imageLink: "\(entry.url)")
+                    .padding(.trailing, 20)
+                
             }
         }
+        
+        
+        
+        
+            .onAppear {
+                PokeApi().getData() { pokemon in
+                    self.pokemon = pokemon
+                }
+            }
     }
-
-
-
+    
+    
+    //    func getSelectedPokemonSprite(url: String) {
+    //        var tempSprite: String?
+    //        PokemonSelectedApi().getData(url: url) { sprite in
+    //            tempSprite = sprite.front_default
+    //        }
+    //
+    //    }
+    
+    //    func getSelectedPokemonData(_: pokemonName) {
+    //
+    //        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/"+pokemonName) else {
+    //
+    //            //if null return out of this block
+    //            return
+    //
+    //        }
+    //
+    //        //only using data
+    //        URLSession.shared.dataTask(with: url) { (data, response, error) in
+    //            guard let data = data else { return }
+    //
+    //            let pokemonList = try! JSONDecoder().decode(Pokemon.self, from: data)
+    //
+    //            DispatchQueue.main.async {
+    //                completion(pokemonList.results)
+    //            }
+    //        }.resume()
+    //    }
+    
+    
+}
