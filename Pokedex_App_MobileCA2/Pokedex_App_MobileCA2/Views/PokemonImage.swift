f6b5ead419b70b853ac44f69bf7274224e2f9a27
deanmaher <72203596+deanmaher@users.noreply.github.com>
2023-05-17 11:17:07 +0100

    Add files via upload

--- Pokedex_App_MobileCA2/Pokedex_App_MobileCA2/Views/PokemonImage.swift
+++ Pokedex_App_MobileCA2/Pokedex_App_MobileCA2/Views/PokemonImage.swift
@@ -4,15 +4,17 @@ import SwiftUI
 struct PokemonImage: View {
     var imageLink = ""
     @State private var pokemonSprite = ""
+    @State private var pokemonWeight = 0
     
     var body: some View {
         AsyncImage(url: URL(string: pokemonSprite))
             .frame(width: 75, height: 75)
             .onAppear {
                 let loadedData = UserDefaults.standard.string(forKey: imageLink)
-                
+                let loadedData2 = UserDefaults.standard.int(forKey: pokemonWeight)
                 if loadedData == nil {
                     getSprite(url: imageLink)
+                    getWeight(url: pokemonWeight)
                     UserDefaults.standard.set(imageLink, forKey: imageLink)
                     //print("New url!!! Caching...")
                 } else {
@@ -27,9 +29,17 @@ struct PokemonImage: View {
         
     }
     
+    func getWeight(url: String){
+        var tempWeight: Int?
+        PokemonGetSpriteApi2().getData(url: url) { weight in
+            tempWeight = weight.weight
+            self.pokemonWeight = tempWeight ?? 9999
+        }
+    }
+    
     func getSprite(url: String) {
         var tempSprite: String?
-        PokemonSelectedApi().getData(url: url) { sprite in
+        PokemonGetSpriteApi().getData(url: url) { sprite in 
             tempSprite = sprite.front_default
             self.pokemonSprite = tempSprite ?? "placeholder"
         }
