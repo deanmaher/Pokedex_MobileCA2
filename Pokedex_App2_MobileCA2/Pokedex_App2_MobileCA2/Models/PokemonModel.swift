c6c286540327600d784550ee875bb48ce31e3a69
deanmaher <72203596+deanmaher@users.noreply.github.com>
2023-05-23 18:59:26 +0100

    Add files via upload

---
+++ Pokedex_App2_MobileCA2/Pokedex_App2_MobileCA2/Models/PokemonModel.swift
@@ -0,0 +1,67 @@
+//
+//  PokemonModel.swift
+//  Pokedex_App2_MobileCA2
+//
+//  Created by Student on 17/05/2023.
+//
+
+import Foundation
+
+struct PokemonPage: Codable {
+    let count: Int
+    let next: String
+    let results: [Pokemon]
+}
+
+struct Pokemon: Codable, Identifiable, Equatable{
+    var id: String {
+        String(url.split(separator: "/").last ?? "")
+    }
+    let name: String
+    let url: String
+}
+
+struct PokemonResponse: Codable {
+    let count: Int
+    let next: String?
+    let previous: String?
+    let results: [Pokemon]
+}
+
+struct DetailPokemon: Codable {
+    let id: Int
+    let height: Int
+    let weight: Int
+    let base_experience: Int
+    let types: [PokemonTypes]
+}
+
+struct PokemonTypes: Codable {
+    let slot: Int
+    let type: TypeData
+}
+
+struct TypeData: Codable {
+    let name: String
+    let url: String
+}
+
+struct TypeResponse: Codable {
+    let results: [Pokemon]
+}
+
+struct PokemonTypeResponse: Codable {
+    let pokemon: [PokemonTypeDetail]
+}
+
+
+struct PokemonTypeDetailResponse: Codable {
+    let pokemon: [PokemonTypeDetail]
+}
+
+struct PokemonTypeDetail: Codable {
+    let pokemon: Pokemon
+}
+
+
+
---
+++ Pokedex_App2_MobileCA2/Pokedex_App2_MobileCA2/Views/PersonalPokedexView.swift
@@ -0,0 +1,41 @@
+//  PersonalPokedexView.swift
+//  Pokedex_App2_MobileCA2
+//
+//  Created by Student on 23/05/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct PersonalPokedexView: View {
+    @Binding var pokemonList2: [PokemonT]
+
+    var body: some View {
+        List(pokemonList2) { pokemon in
+            VStack {
+                HStack {
+                    pokemon.image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 80)
+                        .clipShape(Circle())
+                        .shadow(radius: 10)
+                        .padding(5)
+                    VStack(alignment: .leading) {
+                        Text(pokemon.name)
+                            .font(.headline)
+                        Text(pokemon.type)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                        Text(pokemon.ability)
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                        Text("Age: \(pokemon.age)")
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+        }
+    }
+}
